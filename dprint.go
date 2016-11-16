3faa07cb3868be2502eba74c73a712a53e97741b
Paul Mikesell <pmikesell@gmail.com>
2016-11-16 11:24:05 -0500

    Just some golang debug print helpers

---
+++ dprint.go
@@ -0,0 +1,19 @@
+package common
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+// Dprintf to debug print in red
+func Dprintf(f string, args ...interface{}) {
+	fmt.Printf("\033[48;5;1m")
+	fmt.Printf(f, args...)
+	fmt.Printf("\033[0m\n")
+}
+
+// DSprintf debug print with stack
+func DSprintf(f string, args ...interface{}) {
+	Dprintf(f, args...)
+	debug.PrintStack()
+}
